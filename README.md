643d9b6427caaf559e7db8dd650ea94005525153
“rachelzn” <racheltjengerari@gmail.com>
2024-05-08 18:23:41 +0700

    Experiment 3.2: Be Creative!

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 <img src="image/image-1.png">
 
 After cloning the two repositories from https://github.com/jtordgeman/YewChat/tree/websockets-part2 and https://github.com/jtordgeman/SimpleWebsocketServer, and upon running `npm start` YewChat and the Simple WebSocket Server, we can interact between pages that have entered a username into the application. 
+
+## 3.2. Add Some Creativities to The Webclient
+<img src="image/login.png">
+<img src="image/chat.png">
+
+I modified the theme to follow a dark-mode color palette and added violet accents.
+
+
--- src/components/chat.rs
+++ src/components/chat.rs
@@ -139,21 +139,21 @@ impl Component for Chat {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let submit = ctx.link().callback(|_| Msg::SubmitMessage);
         html! {
-            <div class="flex w-screen">
-                <div class="flex-none w-56 h-screen bg-gray-100">
+            <div class="flex w-screen text-gray-300">
+                <div class="flex-none w-56 h-screen bg-gray-800">
                     <div class="text-xl p-3">{"Users"}</div>
                     {
                         self.users.clone().iter().map(|u| {
                             html!{
-                                <div class="flex m-3 bg-white rounded-lg p-2">
+                                <div class="flex m-3 bg-gray-700 rounded-lg p-2 shadow-lg">
                                     <div>
-                                        <img class="w-12 h-12 rounded-full" src={u.avatar.clone()} alt="avatar"/>
+                                        <img class="w-12 h-12 rounded-full border border-gray-600" src={u.avatar.clone()} alt="avatar"/>
                                     </div>
                                     <div class="flex-grow p-3">
                                         <div class="flex text-xs justify-between">
-                                            <div>{u.name.clone()}</div>
+                                            <div class="text-white">{u.name.clone()}</div>
                                         </div>
-                                        <div class="text-xs text-gray-400">
+                                        <div class="text-xs">
                                             {"Hi there!"}
                                         </div>
                                     </div>
@@ -162,20 +162,20 @@ impl Component for Chat {
                         }).collect::<Html>()
                     }
                 </div>
-                <div class="grow h-screen flex flex-col">
-                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
-                    <div class="w-full grow overflow-auto border-b-2 border-gray-300">
+                <div class="grow h-screen flex flex-col bg-gray-900">
+                    <div class="w-full h-14 border-b-2 border-gray-700"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
+                    <div class="w-full grow overflow-auto border-b-2 border-gray-700">
                         {
                             self.messages.iter().map(|m| {
                                 let user = self.users.iter().find(|u| u.name == m.from).unwrap();
                                 html!{
-                                    <div class="flex items-end w-3/6 bg-gray-100 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg ">
-                                        <img class="w-8 h-8 rounded-full m-3" src={user.avatar.clone()} alt="avatar"/>
+                                    <div class="flex items-end w-3/6 bg-gray-800 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg shadow-lg">
+                                        <img class="w-8 h-8 rounded-full m-3 border border-gray-600" src={user.avatar.clone()} alt="avatar"/>
                                         <div class="p-3">
                                             <div class="text-sm">
                                                 {m.from.clone()}
                                             </div>
-                                            <div class="text-xs text-gray-500">
+                                            <div class="text-xs">
                                                 if m.message.ends_with(".gif") {
                                                     <img class="mt-3" src={m.message.clone()}/>
                                                 } else {
@@ -190,9 +190,9 @@ impl Component for Chat {
 
                     </div>
                     <div class="w-full h-14 flex px-3 items-center">
-                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                        <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
-                            <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
+                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-800 rounded-full outline-none focus:text-gray-100" name="message" required=true />
+                        <button onclick={submit} class="p-3 shadow-lg bg-violet-400 w-10 h-10 rounded-full flex justify-center items-center text-white">
+                            <svg fill="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                             </svg>
                         </button>
